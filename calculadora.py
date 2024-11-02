c81360f653671d35532804a2efd7eb8b743200c1
PAI DO FLÁVIO <49536075+chaotical90@users.noreply.github.com>
2024-11-02 19:33:07 +0000

    Update calculadora.py
    calculadora final

--- calculadora.py
+++ calculadora.py
@@ -15,3 +15,42 @@ def divisao(x, y):
 # Função para multiplicação
 def multiplicacao(x, y):
     return x * y
+    # Função principal
+def calculadora():
+    lista_historico = []
+    while True:
+        print("\nDigite a operação (+, -, *, /), 'hist' para ver o histórico ou 'sair' para encerrar.")
+        op = input("Operação: ")
+
+        if op == "sair":
+            print("Encerrando a calculadora.")
+            break
+        elif op == "hist":
+            print("Histórico de resultados:", lista_historico)
+            continue
+
+        num1 = int(input("Digite o primeiro número: "))
+        num2 = int(input("Digite o segundo número: "))
+
+        if op == '+':
+            resultado = soma(num1, num2)
+            print("Resultado da soma:", resultado)
+            lista_historico.append(resultado)
+        if op == '-':
+            resultado = subtracao(num1, num2)
+            print("Resultado da subtração:", resultado)
+            lista_historico.append(resultado)
+        if op == '/':
+            resultado = divisao(num1, num2)
+            print("Resultado da divisão:", resultado)
+            lista_historico.append(resultado)
+        if op == '*':
+            resultado = multiplicacao(num1, num2)
+            print("Resultado da multiplicação:", resultado)
+            lista_historico.append(resultado)
+        else:
+            print("Operação inválida. Tente novamente.")
+
+# Executa a calculadora
+calculadora()
+
